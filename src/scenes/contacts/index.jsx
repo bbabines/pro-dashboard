c97ab5c7b7bba201befa889ce614d2a8af48ce19
unknown <bbabines@gmail.com>
2023-03-18 22:22:16 -0400

    Contacts section finished.

--- src/scenes/contacts/index.jsx
+++ src/scenes/contacts/index.jsx
@@ -1,15 +1,12 @@
-import { Box, Typography, useTheme } from "@mui/material";
-import { DataGrid } from "@mui/x-data-grid";
-
-import AdminPanelSettingsOutlinedIcon from "@mui/icons-material/AdminPanelSettingsOutlined";
-import LockOpenOutlinedIcon from "@mui/icons-material/LockOpenOutlined";
-import SecurityOutlinedIcon from "@mui/icons-material/SecurityOutlined";
+import { Box } from "@mui/material";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
 
 import { tokens } from "../../theme";
-import { mockDataTeam } from "../../data/mockData";
+import { mockDataContacts } from "../../data/mockData";
+import { useTheme } from "@mui/material";
 import Header from "../../scenes/component/Header";
 
-const Team = () => {
+const Contacts = () => {
 	const theme = useTheme();
 	const colors = tokens(theme.palette.mode);
 
@@ -17,19 +14,21 @@ const Team = () => {
 		{
 			field: "id",
 			headerName: "ID",
+			flex: 0.5,
+		},
+		{
+			field: "registrarId",
+			headerName: "Registrar ID",
 		},
 		{
 			field: "name",
 			headerName: "Name",
-			flex: 1,
 			cellClassName: "name-column--cell",
 		},
 		{
 			field: "age",
 			headerName: "Age",
 			type: "number",
-			handleAlign: "left",
-			align: "left",
 		},
 		{
 			field: "phone",
@@ -42,37 +41,28 @@ const Team = () => {
 			flex: 1,
 		},
 		{
-			field: "access",
-			headerName: "Access Level",
+			field: "address",
+			headerName: "Address",
 			flex: 1,
-			renderCell: ({ row: { access } }) => {
-				return (
-					<Box
-						width="60%"
-						m="0 auto"
-						p="5px"
-						display="flex"
-						justifyContent="center"
-						backgroundColor={
-							access === "admin"
-								? colors.greenAccent[600]
-								: colors.greenAccent[700]
-						}
-						borderRadius="4px"
-					>
-						{access === "admin" && <AdminPanelSettingsOutlinedIcon />}
-						{access === "manager" && <SecurityOutlinedIcon />}
-						{access === "user" && <LockOpenOutlinedIcon />}
-						<Typography color={colors.grey[100]}>{access}</Typography>
-					</Box>
-				);
-			},
+		},
+		{
+			field: "city",
+			headerName: "City",
+			flex: 1,
+		},
+		{
+			field: "zipCode",
+			headerName: "ZipCode",
+			flex: 1,
 		},
 	];
 
 	return (
 		<Box m="20px">
-			<Header title="TEAM" subtitle="Managing the Team Members" />
+			<Header
+				title="CONTACTS"
+				subtitle="List of Contacts for Future Reference"
+			/>
 			<Box
 				m="40px 0 0 0"
 				height="75vh"
@@ -100,12 +90,21 @@ const Team = () => {
 					"& .MuiCheckbox-root": {
 						color: `${colors.greenAccent[200]} !important`,
 					},
+
+					"& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+						color: `${colors.grey[100]} !important`,
+					},
 				}}
 			>
-				<DataGrid checkboxSelection rows={mockDataTeam} columns={columns} />
+				<DataGrid
+					checkboxSelection
+					rows={mockDataContacts}
+					columns={columns}
+					components={{ Toolbar: GridToolbar }}
+				/>
 			</Box>
 		</Box>
 	);
 };
 
-export default Team;
+export default Contacts;
