dc3cc80ca3e59c9442278a1995fbc9060bac94f1
unknown <bbabines@gmail.com>
2023-03-18 20:58:39 -0400

    Added Team section to the dashboard.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Topbar from "./scenes/global/Topbar";
 import Dashboard from "./scenes/dashboard";
 import Sidebar from "./scenes/global/Sidebar";
 
-// import Team from "./scenes/Team";
+import Team from "./scenes/team";
 // import Invoices from "./scenes/dashboard";
 // import Contacts from "./scenes/invoices";
 // import Bar from "./scenes/bar";
@@ -29,10 +29,10 @@ function App() {
 					<main className="content">
 						<Routes>
 							<Route path="/" element={<Dashboard />}></Route>
+							<Route path="/team" element={<Team />} />
 						</Routes>
 						<Topbar />
 						{/* <Route path="/" element={<Dashboard />} /> */}
-						{/* <Route path="/team" element={<Team />} /> */}
 						{/* <Route path="/contacts" element={<Contacts />} /> */}
 						{/* <Route path="/invoices" element={<Invoices />} /> */}
 						{/* <Route path="/form" element={<Form />} /> */}
